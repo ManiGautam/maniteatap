31abfb0e3f2279cbbefe06f8bb4c7a2d6505bad8
manijee <inboxtomanijee@gmail.com>
2021-08-30 13:07:50 +0530

    Initial Commit

---
+++ maniteatap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  maniteatap
+//
+//  Created by Mani Jee on 30/08/21.
+//  Copyright © 2021 Developers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
